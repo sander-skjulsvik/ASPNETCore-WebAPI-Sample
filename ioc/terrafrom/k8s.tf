dee8b2fbcdbfb72eac10d346fa8dfefda6cc2c6b
sander.skjulsvik <sander.skjulsvik@outlook.com>
2022-10-16 17:41:32 +0200

    adding second node to k8s

--- ioc/terrafrom/k8s.tf
+++ ioc/terrafrom/k8s.tf
@@ -14,7 +14,7 @@ resource "azurerm_kubernetes_cluster" "k8s-ASPNETCore-WebAPI-Sample" {
 
   default_node_pool {
     name       = "default"
-    node_count = 1
+    node_count = 2
     vm_size    = "Standard_D2_v2"
   }
 
